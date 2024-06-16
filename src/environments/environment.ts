211c39a1ad2c67e0c50011cdba7d06be1f8bd3dd
Lucas <131302149+LucasRozeno7@users.noreply.github.com>
2024-06-16 18:51:54 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyASGkWtg8xeZ7pTCcY3mCgajbY_namJPtU",
-    authDomain: "pokeprojeto-72ec7.firebaseapp.com",
-    projectId: "pokeprojeto-72ec7",
-    storageBucket: "pokeprojeto-72ec7.appspot.com",
-    messagingSenderId: "1005115795641",
-    appId: "1:1005115795641:web:3a67d021cbdc26a1d39368",
-    measurementId: "G-FFE5ZLBFW3"
+    apiKey: " ",
+    authDomain: " ",
+    projectId: " ",
+    storageBucket: " ",
+    messagingSenderId: " ",
+    appId: " ",
+    measurementId: "  "
   }
 };
 
